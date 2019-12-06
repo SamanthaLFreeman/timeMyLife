7b3f34cf02dcc7c76682daf2d825ccee13f7d035
Alan Turing <freemasl.sf@gmail.com>
2019-12-06 16:21:02 -0700

    Move App and setup initial heading

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Time My Life</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
