b62fcbbaac448307869799801778a42a9df8ea9d
Alan Turing <freemasl.sf@gmail.com>
2019-12-09 16:00:25 -0700

    Add JSX elements for ActivityForm

---
+++ src/ActivityForm/ActivityForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ActivityForm = () => {
+  return (
+    <section>
+      <h2>New Activity</h2>
+      <p>Select a category:</p>
+      <button>Study</button>
+      <button>Meditate</button>
+      <button>Exercise</button>
+      <input placeholder='Name Activity'/>
+      <input placeholder='Minutes' />
+      <input placeholder='Seconds' />
+      <button>Start Activity</button>
+    </section>
+  )
+}
+
+export const ActivityForm;
