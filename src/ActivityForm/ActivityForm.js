044bc97868f237690001038dbf891fe7533f79c3
Alan Turing <freemasl.sf@gmail.com>
2019-12-11 15:18:18 -0700

    Convert ActivityForm and App to class components

--- src/ActivityForm/ActivityForm.js
+++ src/ActivityForm/ActivityForm.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ActivityForm = () => {
-  return (
-    <form className='ActivityForm'>
-      <h2>New Activity</h2>
-      <p>Select a category:</p>
-      <div className='categories'>
-        <button>Study</button>
-        <button>Meditate</button>
-        <button>Exercise</button>
-      </div>
-      <div className='inputs'>
-        <input 
-          placeholder='Name Activity'
-          type='text' />
-        <input 
-          placeholder='Minutes'
-          type='number' />
-        <input 
-          placeholder='Seconds'
-          type='number' />
-        <button>Start Activity</button>
-      </div>
-    </form>
-  )
+class ActivityForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <form className='ActivityForm'>
+        <h2>New Activity</h2>
+        <p>Select a category:</p>
+        <div className='categories'>
+          <button>Study</button>
+          <button>Meditate</button>
+          <button>Exercise</button>
+        </div>
+        <div className='inputs'>
+          <input
+            placeholder='Name Activity'
+            type='text' />
+          <input
+            placeholder='Minutes'
+            type='number' />
+          <input
+            placeholder='Seconds'
+            type='number' />
+          <button>Start Activity</button>
+        </div>
+      </form>
+    )
+  }
 }
 
 export default ActivityForm;
--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ActivityForm from '../ActivityForm/ActivityForm';
 import PastActivities from '../PastActivities/PastActivities';
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Time My Life</h1>
-      </header>
-      <main className="main">
-        <ActivityForm />
-        <PastActivities />
-      </main>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activities: []
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Time My Life</h1>
+        </header>
+        <main className="main">
+          <ActivityForm />
+          <PastActivities />
+        </main>
+      </div>
+    )
+  }
 }
 
 export default App;
