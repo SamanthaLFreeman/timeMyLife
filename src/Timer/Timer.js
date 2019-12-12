3a739855893dbbd1deb5d0d4db85a9e1d814cb31
Alan Turing <freemasl.sf@gmail.com>
2019-12-12 16:06:55 -0700

    Basic structure for Timer component

---
+++ src/Timer/Timer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Timer = () => {
+  return (
+    <section className='ActivityForm'>
+      <h3>Name</h3>
+      <p>Timer</p>
+      <button>Start</button>
+      <button>Cancel Activity</button>
+    </section>
+  )
+}
+
+export default Timer;
