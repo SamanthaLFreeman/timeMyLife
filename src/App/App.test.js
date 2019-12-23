de7919e3fde669b828fc89ae4c76bec019758865
Alan Turing <freemasl.sf@gmail.com>
2019-12-23 14:57:46 -0800

    Setup Enzyme for testing and add App snapshot test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+
+  it('should match the snapshot with the correct data passed through', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+})
