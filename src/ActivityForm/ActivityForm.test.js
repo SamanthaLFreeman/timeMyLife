fa0f8d659e937252e16fa2d7cd163a179c597f6d
Alan Turing <freemasl.sf@gmail.com>
2019-12-29 12:04:15 -0800

    Test the setCurrentActivity method is called on submit of the form with the correct information passecd through in state

--- src/ActivityForm/ActivityForm.test.js
+++ src/ActivityForm/ActivityForm.test.js
@@ -4,9 +4,12 @@ import { shallow } from 'enzyme';
 
 describe('ActivityForm', () => {
   let wrapper;
+  const setCurrentActivityMock = jest.fn();
 
   beforeEach(() => {
-    wrapper = shallow(<ActivityForm />)
+    wrapper = shallow(
+    <ActivityForm 
+      setCurrentActivity={setCurrentActivityMock} />)
   })
 
   it('should match the snapshot with the correct data passed through', () => {
@@ -50,5 +53,47 @@ describe('ActivityForm', () => {
     expect(wrapper.state('name')).toEqual(expectedName);
     expect(wrapper.state('mins')).toEqual(expectedMins);
     expect(wrapper.state('secs')).toEqual(expectedSecs);
+  });
+
+  it('should call the setCurrentActivity method and pass through the correct info when handleSubmit is called on button click', () => {
+    const mockEventCategories = {
+      target: {
+        name: 'categories',
+        value: 'Study'
+      }
+    };
+    const mockEventName = {
+      target: {
+        name: 'name',
+        value: 'Read'
+      }
+    };
+    const mockEventMins = {
+      target: {
+        name: 'mins',
+        value: '10'
+      }
+    };
+    const mockEventSecs = {
+      target: {
+        name: 'secs',
+        value: '30'
+      }
+    };
+    wrapper.instance().handleChange(mockEventCategories);
+    wrapper.instance().handleChange(mockEventName);
+    wrapper.instance().handleChange(mockEventMins);
+    wrapper.instance().handleChange(mockEventSecs);
+    const mockEvent = {
+      preventDefault: jest.fn()
+    };
+    const currentActivityMock = {
+      categories: 'Study',
+      name: 'Read'
+    };
+    const minsMock = 10;
+    const secsMock = 30;
+    wrapper.instance().handleSubmit(mockEvent);
+    expect(setCurrentActivityMock).toHaveBeenCalledWith(currentActivityMock, minsMock, secsMock);
   });
 })
